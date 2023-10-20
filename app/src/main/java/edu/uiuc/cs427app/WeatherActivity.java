ff7eee80335ae352d5aa913b36600b32fe2bf6e5
Yiming <yiming@fcreekcapital.com>
2023-10-20 14:58:15 -0500

    add MapActivity and WeatherActivity

---
+++ app/src/main/java/edu/uiuc/cs427app/WeatherActivity.java
@@ -0,0 +1,13 @@
+package edu.uiuc.cs427app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}

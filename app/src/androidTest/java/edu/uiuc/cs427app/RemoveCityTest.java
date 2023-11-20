630e9e3c1488de2f4382c6ec22566f05e35097d0
hendhany <hend@illinois.edu>
2023-11-19 21:32:01 -0600

    Merge remote-tracking branch 'origin/main' into Milestone_4_documentation_update
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/edu/uiuc/cs427app/MainActivity.java
    #	app/src/main/java/edu/uiuc/cs427app/MapActivity.java
    #	app/src/main/java/edu/uiuc/cs427app/UserDbHelper.java
    #	app/src/main/java/edu/uiuc/cs427app/WeatherActivity.java

---
+++ app/src/androidTest/java/edu/uiuc/cs427app/RemoveCityTest.java
@@ -0,0 +1,244 @@
+package edu.uiuc.cs427app;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class RemoveCityTest {
+
+    @Rule
+    public ActivityScenarioRule<LoginActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(LoginActivity.class);
+
+    @Test
+    public void remove() {
+        ViewInteraction materialTextView = onView(
+                allOf(withId(R.id.register), withText("Register"),
+                        childAtPosition(
+                                childAtPosition(
+                                        allOf(withId(android.R.id.content),
+                                                childAtPosition(
+                                                        withId(androidx.appcompat.R.id.decor_content_parent),
+                                                        0)),
+                                        0),
+                                5),
+                        isDisplayed()));
+        materialTextView.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.username),
+                        childAtPosition(
+                                childAtPosition(
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0),
+                                        2),
+                                1),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("hend"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.password),
+                        childAtPosition(
+                                childAtPosition(
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0),
+                                        3),
+                                1),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("123"), closeSoftKeyboard());
+
+        ViewInteraction materialRadioButton = onView(
+                allOf(withId(R.id.theme2RadioButton), withText("Theme 2"),
+                        childAtPosition(
+                                allOf(withId(R.id.themeRadioGroup),
+                                        childAtPosition(
+                                                childAtPosition(
+                                                        withId(android.R.id.content),
+                                                        0),
+                                                6)),
+                                1),
+                        isDisplayed()));
+        materialRadioButton.perform(click());
+
+        ViewInteraction materialCheckBox = onView(
+                allOf(withId(R.id.checkBox1), withText("Champaign"),
+                        childAtPosition(
+                                childAtPosition(
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0),
+                                        4),
+                                0),
+                        isDisplayed()));
+        materialCheckBox.perform(click());
+
+        ViewInteraction materialCheckBox2 = onView(
+                allOf(withId(R.id.checkBox2), withText("Chicago"),
+                        childAtPosition(
+                                childAtPosition(
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0),
+                                        4),
+                                1),
+                        isDisplayed()));
+        materialCheckBox2.perform(click());
+
+        ViewInteraction materialCheckBox3 = onView(
+                allOf(withId(R.id.checkBox3), withText("New York"),
+                        childAtPosition(
+                                childAtPosition(
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0),
+                                        4),
+                                2),
+                        isDisplayed()));
+        materialCheckBox3.perform(click());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.register), withText("Register"),
+                        childAtPosition(
+                                childAtPosition(
+                                        allOf(withId(android.R.id.content),
+                                                childAtPosition(
+                                                        withId(androidx.appcompat.R.id.decor_content_parent),
+                                                        0)),
+                                        0),
+                                7),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withText("New York"),
+                        withParent(withParent(allOf(withId(R.id.cityButtonLayout),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))))),
+                        isDisplayed()));
+        textView.check(matches(withText("New York")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withText("New York"),
+                        withParent(withParent(allOf(withId(R.id.cityButtonLayout),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))))),
+                        isDisplayed()));
+        textView2.check(matches(withText("New York")));
+
+        ViewInteraction textView3 = onView(
+                allOf(withText("Chicago"),
+                        withParent(withParent(allOf(withId(R.id.cityButtonLayout),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))))),
+                        isDisplayed()));
+        textView3.check(matches(withText("Chicago")));
+
+        ViewInteraction textView4 = onView(
+                allOf(withText("Chicago"),
+                        withParent(withParent(allOf(withId(R.id.cityButtonLayout),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))))),
+                        isDisplayed()));
+        textView4.check(matches(withText("Chicago")));
+
+        ViewInteraction textView5 = onView(
+                allOf(withText("Chicago"),
+                        withParent(withParent(allOf(withId(R.id.cityButtonLayout),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))))),
+                        isDisplayed()));
+        textView5.check(matches(withText("Chicago")));
+
+        // WAIT
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        ViewInteraction button = onView(
+                allOf(withText("Remove"),
+                        childAtPosition(
+                                childAtPosition(
+                                        allOf(withId(R.id.cityButtonLayout),
+                                                childAtPosition(
+                                                        withClassName(is("android.widget.LinearLayout")),
+                                                        2)),
+                                        0),
+                                3),
+                        isDisplayed()));
+        button.perform(click());
+
+        // WAIT
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        ViewInteraction button2 = onView(
+                allOf(withText("Remove"),
+                        childAtPosition(
+                                childAtPosition(
+                                        allOf(withId(R.id.cityButtonLayout),
+                                                childAtPosition(
+                                                        withClassName(is("android.widget.LinearLayout")),
+                                                        2)),
+                                        0),
+                                3),
+                        isDisplayed()));
+        button2.perform(click());
+
+        // WAIT
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
